4697d5d06d1c253cbd020f617f1524fb1b293c6a
ManewJ <manoeljustino@gmail.com>
2025-05-26 11:58:36 -0300

    Adicionando estrutura inicial do projeto

---
+++ src/main/java/com/mercadinho/mercadinho_uniesp/MercadinhoUniespApplication.java
@@ -0,0 +1,13 @@
+package com.mercadinho.mercadinho_uniesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  MercadinhoUniespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadinhoUniespApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadinho/mercadinho_uniesp/MercadinhoUniespApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadinho.mercadinho_uniesp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadinhoUniespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
